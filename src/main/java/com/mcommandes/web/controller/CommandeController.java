860276704a79f358ee3c426e3808b60dbd8c9251
Yassine_TAIR <yassine1tair@gmail.com>
2019-12-23 14:57:13 +0100

    YTR_First_commit 1

---
+++ src/main/java/com/mcommandes/web/controller/CommandeController.java
@@ -0,0 +1,45 @@
+package com.mcommandes.web.controller;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mcommandes.dao.CommandesDao;
+import com.mcommandes.model.Commande;
+import com.mcommandes.web.exceptions.CommandeNotFoundException;
+import com.mcommandes.web.exceptions.ImpossibleAjouterCommandeException;
+
+@RestController
+public class CommandeController {
+
+    @Autowired
+    CommandesDao commandesDao;
+
+    @PostMapping (value = "/commandes")
+    public ResponseEntity<Commande> ajouterCommande(@RequestBody Commande commande){
+
+        Commande nouvelleCommande = commandesDao.save(commande);
+
+        if(nouvelleCommande == null) throw new ImpossibleAjouterCommandeException("Impossible d'ajouter cette commande");
+
+        return new ResponseEntity<Commande>(commande, HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/commandes/{id}")
+    public Optional<Commande> recupererUneCommande(@PathVariable int id){
+
+        Optional<Commande> commande = commandesDao.findById(id);
+
+        if(!commande.isPresent()) throw new CommandeNotFoundException("Cette commande n'existe pas");
+
+        return commande;
+    }
+}
